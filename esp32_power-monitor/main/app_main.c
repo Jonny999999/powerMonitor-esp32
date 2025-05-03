a661336f44b16fbbe054be584a7bda4db234930f
jonny <jonny@wwad.de>
2025-05-03 10:47:45 +0200

    Add library for pzem004t - readout works (example)

--- esp32_power-monitor/main/app_main.c
+++ esp32_power-monitor/main/app_main.c
@@ -12,6 +12,9 @@
 #include <arpa/inet.h> 
 
 
+#include "pzem004tv3.h"
+
+
 
 //===== CONFIG =====
 #define WIFI_SSID "E14_mqtt-broker"
@@ -198,28 +201,76 @@ static void publish_poti_task(void *arg) {
 
 
 
-void app_main(void) {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-    ESP_LOGW(TAG, "Starting wifi...");
-    nvs_flash_init();
-    wifi_init_sta();
-    vTaskDelay(pdMS_TO_TICKS(6000));
 
-    ESP_LOGW(TAG, "Configuring gpios...");
-    gpio_reset_pin(BUZZER_GPIO);
-    gpio_set_direction(BUZZER_GPIO, GPIO_MODE_OUTPUT);
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_11);
 
-    ESP_LOGW(TAG, "Starting mqtt...");
-    mqtt_app_start();
-    vTaskDelay(pdMS_TO_TICKS(3000));
+pzem_setup_t pzConf =
+{
+    .pzem_uart   = UART_NUM_2,              /*  <== Specify the UART you want to use, UART_NUM_0, UART_NUM_1, UART_NUM_2 (ESP32 specific) */
+    .pzem_rx_pin = GPIO_NUM_16,             /*  <== GPIO for RX */
+    .pzem_tx_pin = GPIO_NUM_17,             /*  <== GPIO for TX */
+    .pzem_addr   = PZ_DEFAULT_ADDRESS,      /*  If your module has a different address, specify here or update the variable in pzem004tv3.h */
+};
 
-    ESP_LOGW(TAG, "Starting publish task...");
-    xTaskCreate(publish_poti_task, "publish_poti_task", 4096, NULL, 5, NULL);
+TaskHandle_t PMonTHandle = NULL;
+_current_values_t pzValues;            /* Measured values */
+
+void PMonTask( void * pz );
+
+void app_main()
+{
+    /* Initialize/Configure UART */
+    PzemInit( &pzConf );
+
+    xTaskCreate( PMonTask, "PowerMon", ( 256 * 8 ), NULL, tskIDLE_PRIORITY, &PMonTHandle );
 }
+
+
+
+void PMonTask( void * pz )
+{
+    for( ;; )
+    {
+        PzemGetValues( &pzConf, &pzValues );
+        printf( "Vrms: %.1fV - Irms: %.3fA - P: %.1fW - E: %.2fWh\n", pzValues.voltage, pzValues.current, pzValues.power, pzValues.energy );
+        printf( "Freq: %.1fHz - PF: %.2f\n", pzValues.frequency, pzValues.pf );
+
+        ESP_LOGI( TAG, "Stack High Water Mark: %ld Bytes free", ( unsigned long int ) uxTaskGetStackHighWaterMark( NULL ) );     /* Show's what's left of the specified stacksize */
+
+        vTaskDelay( pdMS_TO_TICKS( 2500 ) );
+    }
+
+    vTaskDelete( NULL );
+}
+
+
+
+
+
+//void app_main(void) {
+//    ESP_LOGI(TAG, "[APP] Startup..");
+//    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+//    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+//
+//    //ESP_LOGW(TAG, "Starting wifi...");
+//    //nvs_flash_init();
+//    //wifi_init_sta();
+//    //vTaskDelay(pdMS_TO_TICKS(6000));
+//
+//    //ESP_LOGW(TAG, "Configuring gpios...");
+//    //gpio_reset_pin(BUZZER_GPIO);
+//    //gpio_set_direction(BUZZER_GPIO, GPIO_MODE_OUTPUT);
+//    //adc1_config_width(ADC_WIDTH_BIT_12);
+//    //adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_11);
+//
+//    //ESP_LOGW(TAG, "Starting mqtt...");
+//    //mqtt_app_start();
+//    //vTaskDelay(pdMS_TO_TICKS(3000));
+//
+//    //ESP_LOGW(TAG, "Starting publish task...");
+//    //xTaskCreate(modbus_sensor_task, "modbus_sensor_task", 4096, NULL, 5, NULL);
+//
+//}
+
 
 
